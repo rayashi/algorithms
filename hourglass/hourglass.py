2879f5dc102919342cbabe41c807948c76057799
Humberto Dib Rayashi <rayashi@gmail.com>
2018-06-11 11:30:51 -0300

    Solution for hourglass problem, adding more test case

--- hourglass/hourglass.py
+++ hourglass/hourglass.py
@@ -1,2 +1,25 @@
+
+def sum_sub_matrix(matrix, initial_line, initial_column, size):
+    waist = False
+    sum = 0
+    for line in range(initial_line, initial_line + size):
+        if not waist:
+            for column in range(initial_column, initial_column + size):
+                try:
+                    sum += matrix[line][column]
+                except IndexError:
+                    pass
+        else:
+            sum += matrix[line][initial_column+1]
+        waist = not waist
+    return sum
+
+
 def hourglass(matrix):
-    pass
+    max_sum = -9**2
+    for line in range(4):
+        for column in range(4):
+            sum = sum_sub_matrix(matrix, line, column, 3)
+            if sum > max_sum:
+                max_sum = sum
+    return max_sum
--- hourglass/hourglass_test.py
+++ hourglass/hourglass_test.py
@@ -1,4 +1,3 @@
-import timeit
 import unittest
 from hourglass.hourglass import *
 
@@ -14,4 +13,21 @@ class TestHourglass(unittest.TestCase):
             [0, 0, 1, 2, 4, 0]
         ]
         self.assertEqual(hourglass(matrix), 19)
-
+        matrix = [
+            [1, 1, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [1, 1, 1, 0, 0, 0],
+            [0, 9, 2, -4, -4, 0],
+            [0, 0, 0, -2, 0, 0],
+            [0, 0, -1, -2, -4, 0],
+        ]
+        self.assertEqual(hourglass(matrix), 13)
+        matrix = [
+            [-1, -1, 0, -9, -2, -2],
+            [-2, -1, -6, -8, -2, -5],
+            [-1, -1, -1, -2, -3, -4],
+            [-1, -9, -2, -4, -4, -5],
+            [-7, -3, -3, -2, -9, -9],
+            [-1, -3, -1, -2]
+        ]
+        self.assertEqual(hourglass(matrix), -6)
