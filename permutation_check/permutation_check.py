ec4c8d8a576ff8ef627c421c8009be5cdd812e39
Humberto Dib Rayashi <rayashi@gmail.com>
2018-06-12 11:00:11 -0300

    First solution for permutation check, using dictionary

--- permutation_check/permutation_check.py
+++ permutation_check/permutation_check.py
@@ -1,3 +1,13 @@
 
 def permutation_check(numbers):
-    pass
+    increments = {}
+    for number in numbers:
+        increments[number] = number + 1
+
+    for number in numbers:
+        try:
+            increments[number+1]
+        except KeyError:
+            if number != len(numbers):
+                return 0
+    return 1
--- permutation_check/permutation_check_test.py
+++ permutation_check/permutation_check_test.py
@@ -7,6 +7,8 @@ class TestPermutationCheck(unittest.TestCase):
     def test_test_permutation_check(self):
         self.assertEqual(permutation_check([4, 1, 3, 2]), 1)
         self.assertEqual(permutation_check([4, 1, 3]), 0)
+        self.assertEqual(permutation_check([4, 3, 2]), 0)
+        self.assertEqual(permutation_check([4, 3, 2, 3]), 0)
         t = timeit.Timer(lambda: permutation_check([4, 1, 3, 2])).timeit(500)
         print('Permutation Check performance: %s' % t)
 
