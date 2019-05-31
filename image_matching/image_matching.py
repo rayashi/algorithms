e01c5cd1a16529e27135a1b90b3dfaba1b893972
Humberto Dib <humbertodib@cocaudi.com.br>
2019-05-31 15:26:13 -0300

    Finished Image Matchs solution

--- image_matching/image_matching.py
+++ image_matching/image_matching.py
@@ -1,21 +1,51 @@
 
 def countMatches(grid1, grid2):
-  pass
+  count = 0
+  regions1 = makeRegions(grid1)
+  regions2 = makeRegions(grid2)
+  for reg1 in regions1:
+    for reg2 in regions2:
+      if reg1 == reg2:
+        count += 1
+  return count
+
 
 def makeRegions(grid):
   regions = []
   for line_index, line in enumerate(grid):
-    for bit in enumerate(line):
+    for colunm_index, bit in enumerate(line):
       if bit == '1':
-        pass    
-
-  pass
+        regions = getAdjacentsRegions((line_index, colunm_index), regions)
+  return regions
 
 
 def getAdjacentsRegions(position, regions):
-  for region in regions:
+  if not regions:
+    return [{ position }]
+  adjacents_regions = []
+
+  # Find adjacents regions
+  for region_index, region in enumerate(regions):
     for dot in region:
-      pass
+      if isAdjacent(position, dot):
+        adjacents_regions.append(region_index) 
+        break
+
+  if adjacents_regions:
+    return mergeAdjacentsRegions(regions, adjacents_regions, position)
+  else:
+    return regions + [{position}] 
+
+
+def mergeAdjacentsRegions(regions, adjacents_regions, position):
+  new_region = set()
+  for adjacent_region in adjacents_regions:
+    new_region.update(regions[adjacent_region])
+  
+  new_region.add(position)
+  new_regions = [x for i,x in enumerate(regions) if i not in adjacents_regions]
+  new_regions.append(new_region)
+  return new_regions
 
 
 def isAdjacent(position1, position2):
--- image_matching/image_matching_test.py
+++ image_matching/image_matching_test.py
@@ -4,9 +4,15 @@ from image_matching.image_matching import *
 
 class TestMissingInterger(unittest.TestCase):
     def test_countMatches(self):
-        self.assertEqual(countMatches(['001', '011', '100'], ['001', '011', '101']), None)
-        self.assertEqual(countMatches(['111', '100', '100'], ['111', '100', '101']), None)
-
+        self.assertEqual(countMatches(['001', '011', '100'], ['001', '011', '101']), 1)
+        self.assertEqual(countMatches(['111', '100', '100'], ['111', '100', '101']), 1)
+        img1 = ['0100','1001','0011','0011']
+        img2 = ['0101','1001','0011','0011']
+        self.assertEqual(countMatches(img1, img2), 2)
+        img1 = ['0010','0111','0101','1111','0111']
+        img2 = ['0010','0111','0111','1111','1111']
+        self.assertEqual(countMatches(img1, img2), 0)
+               
     def test_is_adjacent(self):
         self.assertEqual(isAdjacent((1,1),(0,0)), False)
         self.assertEqual(isAdjacent((1,1),(0,1)), True)
